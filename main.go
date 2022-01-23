3605da14b555e986f8d786928381b51286372229
garrett-thompson <garrmt@gmail.com>
2022-01-23 17:30:31 -0500

    prompt a user for their data, then calculate & display their power zones.

--- main.go
+++ main.go
@@ -2,19 +2,51 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 
 	"github.com/manifoldco/promptui"
 )
 
 func main() {
+	// collect data
+	ftpValue := promptFtp()
 	weightUnit := promptWeightUnit()
-
 	promptWeightValue(weightUnit)
+
+	// display power zones
+	powerZones := calculatePowerZones(ftpValue)
+	printPowerZones(powerZones)
+
+	// TODO: display watts/kg
+}
+
+func promptFtp() float64 {
+	ftpPrompt := promptui.Prompt{
+		Label: "What is your FTP?",
+	}
+
+	// validate ftp is a number
+	ftpStringValue, promptErr := ftpPrompt.Run()
+
+	ftpValue, parseIntErr := strconv.ParseFloat(ftpStringValue, 64)
+
+	if parseIntErr != nil {
+		fmt.Printf("Failed to parse FTP value %v\n", parseIntErr)
+		// TODO: throw error?
+	}
+
+	if promptErr != nil {
+		fmt.Printf("Failed to get FTP value %v\n", promptErr)
+		// TODO: throw error?
+	}
+
+	return ftpValue
 }
 
 func promptWeightUnit() string {
 	weightUnitPrompt := promptui.Select{
-		Label: "We'll need to get your weight. Do you use the metric system (kgs), or the imperial system (lbs)?",
+		Label: "Next we'll need to get your weight. Do you use the metric system (kgs), or the imperial system (lbs)?",
 		Items: []string{"Metric", "Imperial"},
 	}
 
@@ -36,6 +68,7 @@ func promptWeightValue(unit string) string {
 		Label: fmt.Sprintf("Thanks. What is your weight in %v?", unit),
 	}
 
+	// validate weight is a number
 	weightValue, err := weightValuePrompt.Run()
 
 	if err != nil {
@@ -44,4 +77,45 @@ func promptWeightValue(unit string) string {
 	}
 
 	return weightValue
+}
+
+// TODO: extract this type so we can DRY this up
+func calculatePowerZones(ftp float64) [][]int {
+	zone1Max := int(math.Round(ftp * float64(0.55)))
+	zone2Max := int(math.Round(ftp * float64(0.75)))
+	zone3Max := int(math.Round(ftp * float64(0.90)))
+	zone4Max := int(math.Round(ftp * float64(1.05)))
+	zone5Max := int(math.Round(ftp * float64(1.20)))
+	zone6Max := int(math.Round(ftp * float64(1.50)))
+
+	// TODO: extract this type so we can DRY this up
+	return [][]int{
+		{0, zone1Max},
+		{zone1Max + 1, zone2Max},
+		{zone2Max + 1, zone3Max},
+		{zone3Max + 1, zone4Max},
+		{zone4Max + 1, zone5Max},
+		{zone5Max + 1, zone6Max},
+		{zone6Max + 1, 0},
+	}
+}
+
+func printPowerZones(powerZones [][]int) {
+	for zone := range powerZones {
+		// can you destructure these in a 1-liner similar to JS?
+		min := powerZones[zone][0]
+		max := powerZones[zone][1]
+
+		var zoneRange string
+
+		if min == 0 {
+			zoneRange = "< " + strconv.Itoa(max)
+		} else if max == 0 {
+			zoneRange = "> " + strconv.Itoa(min)
+		} else {
+			zoneRange = strconv.Itoa(min) + " - " + strconv.Itoa(max)
+		}
+
+		fmt.Println("Zone " + strconv.Itoa(zone+1) + ": " + zoneRange)
+	}
 }
